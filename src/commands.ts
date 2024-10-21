37ba146743fb8f361ac9bcd3e79c3ab028d1ead3
Intcognito <130778504+Intcognito@users.noreply.github.com>
2024-10-21 16:39:49 +0200

    showing message to user when list command has arguments

--- src/commands.ts
+++ src/commands.ts
@@ -2,6 +2,10 @@ import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
+  if (args) {
+    console.log(`The "list" command doesn't need any arguments!`);
+    return;
+  }
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
   const formatted = recipes
