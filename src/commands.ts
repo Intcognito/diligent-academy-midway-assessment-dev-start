356f948f8d1dc146079080dcfaaf9d2812f1864e
Intcognito <130778504+Intcognito@users.noreply.github.com>
2024-10-21 16:56:47 +0200

    unfinished implementation of details command

--- src/commands.ts
+++ src/commands.ts
@@ -1,3 +1,4 @@
+import { AppError } from "./app.error";
 import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
@@ -13,4 +14,16 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
     .join('\n');
   console.log('Your recipes:');
   console.log(formatted);
+}
+
+export async function details(store: Store<RecipeType[]>, input: string[]) {
+  if (typeof parseInt(input[0]) !== 'number') {
+    throw new AppError()
+  }
+  const id = parseInt(input[0]);
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+  const searchedRecipe = recipes.find((item) => {item.id === id});
+
+  console.log(`Your recipe:\nID: ${searchedRecipe?.id}\nName: ${searchedRecipe?.name}`);
 }
